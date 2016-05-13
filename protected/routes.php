a214309cd9846fc062cf5c9e651f08ff30d1cecd
Maxim Frolov <consumer2@mail.ru>
2016-05-13 21:43:24 +0500

    + правило роутинга для отправки данных из формы

--- protected/routes.php
+++ protected/routes.php
@@ -4,5 +4,6 @@ return [
     '/news' => '//News/All',
     '/news/last' => '//Index/Default',
     '/news/form' => '//News/Form',
+    '/news/add' => '//News/Add',
     '/news/<1>' => '//News/One(id=<1>)',
 ];
