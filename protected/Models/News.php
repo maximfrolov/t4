a0d0d7a684422fedd7acd2348e763b17599b677e
Maxim Frolov <consumer2@mail.ru>
2016-05-16 00:41:41 +0500

    создал модель новостей

--- protected/Models/News.php
+++ protected/Models/News.php
@@ -2,58 +2,27 @@
 
 namespace App\Models;
 
-use T4\Core\Config;
+use T4\Orm\Model;
 
 /**
  * Class News
  * @package App\Models
  */
 class News
-    extends Config
+    extends Model
 {
 
     /**
-     * Константа класса - Путь к данным.
+     * @property array Схема таблицы БД
      */
-    const PATH_TO_DB = ROOT_PATH_PROTECTED . DS . 'dbNews.php';
-
-    /**
-     * Метод для нахождения всех новостей.
-     * @return array
-     */
-    public static function findAll()
-    {
-        return (new self(self::PATH_TO_DB))->news->toArray();
-    }
-
-    /**
-     * Метод для сортировки всех новостей в обратном порядке.
-     * @return array
-     */
-    public static function findAllDesc()
-    {
-        $arrayNews = self::findAll();
-        krsort($arrayNews);
-        return $arrayNews;
-    }
-
-    /**
-     * Метод для нахождения одной новости по id.
-     * @param int $id
-     * @return Article
-     */
-    public static function findOne(int $id)
-    {
-        return new Article(self::findAll()[$id - 1]);
-    }
-
-    /**
-     * Метод для получения последней новости.
-     * @return Article
-     */
-    public static function getLast()
-    {
-        return new Article(end(self::findAll()));
-    }
+    protected static $schema = [
+        'table' => 'news',
+        'columns' => [
+            'title' => ['type' => 'text'],
+            'text' => ['type' => 'text'],
+            'date' => ['type' => 'date'],
+            'image' => ['type' => 'string'],
+        ],
+    ];
 
 }
