9638bcdd2295f775bc12b405625124282bb3cedd
Maxim Frolov <consumer2@mail.ru>
2016-05-15 18:24:13 +0500

    создал миграцию

---
+++ protected/Migrations/m_1463317497_createNews.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Migrations;
+
+use T4\Orm\Migration;
+
+class m_1463317497_createNews
+    extends Migration
+{
+
+    public function up()
+    {
+    }
+
+    public function down()
+    {
+    }
+    
+}
