6cf199a39c10c231d12a9f78cbd7503854975c8e
Maxim Frolov <consumer2@mail.ru>
2016-05-16 12:34:32 +0500

    + Контроллер News

---
+++ protected/Modules/Admin/Controllers/News.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Modules\Admin\Controllers;
+
+use App\Models\Article;
+use T4\Mvc\Controller;
+
+class News
+    extends Controller
+{
+
+    /**
+     * Метод-экшн для вывода таблицы новостей.
+     */
+    public function actionTable()
+    {
+
+    }
+
+    /**
+     * Метод-экшн для вывода формы добавления новости.
+     */
+    public function actionForm()
+    {
+
+    }
+
+    /**
+     * Метод-экшн для добавления новости.
+     * @param Article $data
+     */
+    public function actionAdd(Article $data)
+    {
+
+    }
+
+
+}
