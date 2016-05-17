4f08bc7b8680a95f931762d7d4a7b4d611313fc5
Maxim Frolov <consumer2@mail.ru>
2016-05-17 11:56:02 +0500

    + правило роутинга

--- protected/routes.php
+++ protected/routes.php
@@ -7,4 +7,5 @@ return [
     '/news/<1>' => '//News/One(id=<1>)',
     '/admin/news' => '/Admin/News/Table',
     '/admin/news/delete/<2>' => '/Admin/News/Delete(id=<2>)',
+    '/admin/news/edit/<3>' => '/Admin/News/Edit(id=<3>)',
 ];
